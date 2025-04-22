9bab72a200535e903fe770df80ecaf4b8cec4c0d
Felipe Carlos <efelipecarlos@gmail.com>
2025-04-22 07:29:32 -0300

    app: disable basepath

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: '/dkwg/booth',
-  assetPrefix: '/dkwg/booth',
+  // basePath: '/dkwg/booth',
+  // assetPrefix: '/dkwg/booth',
   output: 'standalone',
   images: {
     domains: ['earthobservations.org'],
